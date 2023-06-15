0cc4ff43c12404ea2e321e69972c2b3e5a68b872
christian-jesus <christian.cuarite@tecsup.edu.pe>
2023-06-15 11:59:48 -0500

    lab10 completado

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function Fotos()
+    {
+        return $this->hasMany(Fotos::class);
+    }
+
+    public function Comentarios()
+    {
+        return $this->hasMany(Comentarios::class);
+    }
 }
